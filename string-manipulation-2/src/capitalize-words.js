768e9d910a1fdd898822d0e765424b71e821654f
Daniel Pham <dnypham@gmail.com>
2021-08-30 15:10:45 -0700

    Work continues.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,4 +1,14 @@
 /* exported capitalizeWords */
 function capitalizeWords(string) {
+  var words = string.split(' ');
+  var capitalizedArray = [];
 
+  for (var i = 0; i < words.length; i++) {
+    var char1 = words[i][0].toUpperCase();
+    var char2 = words[i].slice(1).toLowerCase();
+    var word = char1 + char2;
+
+    capitalizedArray.push(word);
+  }
+  return capitalizedArray.join(' ');
 }
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,4 @@
 /* exported numVowels */
+function numVowels(string) {
+
+}
