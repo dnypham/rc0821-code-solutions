8c8791b1f38ad02800ba001219b3d855d4a54af7
Daniel Pham <dnypham@gmail.com>
2021-08-31 10:16:47 -0700

    Work continues.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,11 @@
 /* exported swapChars */
+function swapChars(firstIndex, secondIndex, string) {
+  var index1 = string.charAt(firstIndex);
+  var index2 = string.charAt(secondIndex);
+  var string1 = '';
+  var string2 = '';
+
+  string1 = string.replace(index1, index2);
+  string2 = string1.replace(index2, index1);
+  return string2;
+}
