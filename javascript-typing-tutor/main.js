56d6dc5d0ba1523506d24884dd3e242955496a71
Daniel Pham <dnypham@gmail.com>
2021-09-01 13:02:23 -0700

    Work continues.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,5 +1,6 @@
 var $spanList = document.querySelectorAll('span');
 var $paragraph = document.querySelector('p');
+var $button = document.querySelector('button');
 var index = 0;
 
 function handleKeyDown(event) {
@@ -7,14 +8,24 @@ function handleKeyDown(event) {
 
   if ($spanList.length === index) {
     $paragraph.textContent = 'You got 100% correct!';
+    alert('If you would like to play again, click the RESET button!');
   } else if ($spanList[index].textContent === key) {
     $spanList[index].className = 'correct';
     index++;
     $paragraph.textContent = 'You got ' + index / $spanList.length * 100 + '% correct!';
-    $spanList[index].className = 'current';
   } else if ($spanList[index].textContent !== key) {
     $spanList[index].className = 'incorrect';
+  }
+
+  if ($spanList.length > index) {
+    $spanList[index].className = 'current';
   }
 }
 
 document.addEventListener('keydown', handleKeyDown);
+
+function handleClick(event) {
+  location.reload();
+}
+
+$button.addEventListener('click', handleClick);
