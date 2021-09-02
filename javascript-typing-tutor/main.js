85a9e5dcc79b616464f895278696d310ef7c5aec
Daniel Pham <dnypham@gmail.com>
2021-09-01 18:05:12 -0700

    Work continues.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -5,27 +5,23 @@ var index = 0;
 
 function handleKeyDown(event) {
   var key = event.key;
+  var letters = $spanList;
+  var percent = $paragraph;
 
-  if ($spanList.length === index) {
-    $paragraph.textContent = 'You got 100% correct!';
-    alert('If you would like to play again, click the RESET button!');
-  } else if ($spanList[index].textContent === key) {
-    $spanList[index].className = 'correct';
+  if (letters.length === index) {
+    percent.textContent = 'You got 100% correct!';
+  } else if (letters[index].textContent === key) {
+    letters[index].className = 'correct';
     index++;
-    $paragraph.textContent = 'You got ' + index / $spanList.length * 100 + '% correct!';
-  } else if ($spanList[index].textContent !== key) {
-    $spanList[index].className = 'incorrect';
-  }
-
-  if ($spanList.length > index) {
-    $spanList[index].className = 'current';
+    percent.textContent = 'You got ' + index / letters.length * 100 + '% correct!';
+    letters[index].className = 'current';
+  } else if (letters[index].textContent !== key) {
+    letters[index].className = 'incorrect';
   }
 }
 
 document.addEventListener('keydown', handleKeyDown);
 
-function handleClick(event) {
+$button.addEventListener('click', function () {
   location.reload();
-}
-
-$button.addEventListener('click', handleClick);
+});
