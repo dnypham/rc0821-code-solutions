24302197e657405c491b1918586a831a954d3a19
Daniel Pham <dnypham@gmail.com>
2021-09-16 11:31:54 -0700

    Still working.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -3,8 +3,7 @@
 function defaults(target, source) {
 
   for (var key in source) {
-    console.log(key);
-    console.log(source[key]);
-    console.log(source.key[key]);
+
+    Object.assign(target, source);
   }
 }
