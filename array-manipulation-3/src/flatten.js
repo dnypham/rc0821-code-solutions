d325ee80be11b0db7177458fc8e347e5eede9145
Daniel Pham <dnypham@gmail.com>
2021-10-07 22:57:19 +0000

    Work continues on 'flatten'.

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,15 @@
 /* exported flatten */
+
+function flatten(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let i = 0; i < array[i].length; i++) {
+        newArray.push(array[i]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
