8360224557e3e23ad60bd7f9d27124f66f2ff64c
Daniel Pham <dnypham@gmail.com>
2021-10-07 19:04:47 +0000

    Completed node-notes-cli exercise.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -5,6 +5,12 @@ const command = process.argv[2];
 const ID = notebook.nextId;
 const notes = notebook.notes;
 
+function updateNotebook() {
+  fs.writeFile('data.json', JSON.stringify(notebook, null, 2), err => {
+    if (err) throw err;
+  });
+}
+
 if (command === 'read') {
   for (const property in notes) {
     console.log(`${property}: ${notes[property]}`);
@@ -13,9 +19,7 @@ if (command === 'read') {
   notes[ID] = process.argv[3];
   notebook.nextId++;
 
-  fs.writeFile('data.json', JSON.stringify(notebook, null, 2), err => {
-    if (err) throw err;
-  });
+  updateNotebook();
 } else if (command === 'update') {
   for (const property in notes) {
     if (property === process.argv[3]) {
@@ -23,9 +27,12 @@ if (command === 'read') {
     }
   }
 
-  fs.writeFile('data.json', JSON.stringify(notebook, null, 2), err => {
-    if (err) throw err;
-  });
+  updateNotebook();
 } else if (command === 'delete') {
-
+  for (const property in notes) {
+    if (property === process.argv[3]) {
+      delete notes[property];
+    }
+  }
+  updateNotebook();
 }
