b5baa26377b6e14f9952ae01373bd724fe8f87f2
Daniel Pham <dnypham@gmail.com>
2021-10-11 17:37:40 +0000

    Work continues on 'reverse-words' problem.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,13 @@
 /* exported reverseWords */
+
+function reverseWords(string) {
+  let string1 = string;
+  const substring = '';
+
+  for (let i = 0; i < string1.length; i++) {
+    if (string1.charAt(i) === ' ') {
+      console.log(string1.substring(0, string1[i]));
+      string1 = string1.slice(0, string1[i]);
+    }
+  }
+}
