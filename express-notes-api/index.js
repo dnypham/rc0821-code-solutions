0e019e9c05a2f6f5cf536022d15c96d903db7a79
Daniel Pham <dnypham@gmail.com>
2021-10-11 18:58:37 +0000

    Refactor GET by 'id'.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -20,10 +20,10 @@ app.get('/api/notes/:id', (req, res) => {
 
   if (id < 1 || !Number.isInteger(id)) {
     res.status(400).json({ error: 'id must be a positive integer' });
-  } else if (notebook.notes[id].content) {
+  } else if (notebook.notes[id]) {
     res.status(200).json(notebook.notes[id]);
   } else {
-    res.status(404).json({ error: `cannot find note with id ${req.params.id} ` });
+    res.status(404).json({ error: `cannot find note with id ${id} ` });
   }
 });
 
