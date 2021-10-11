2b22cecab077ab0f21fa3c1e1031b10b5e5dcdd1
Daniel Pham <dnypham@gmail.com>
2021-10-11 18:51:51 +0000

    Add functionality for user to view notes based on 'id'.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -16,8 +16,14 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  if (req.params.id < 1 || !Number.isInteger(req.params.id)) {
+  const id = parseInt(req.params.id);
+
+  if (id < 1 || !Number.isInteger(id)) {
     res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (notebook.notes[id].content) {
+    res.status(200).json(notebook.notes[id]);
+  } else {
+    res.status(404).json({ error: `cannot find note with id ${req.params.id} ` });
   }
 });
 
