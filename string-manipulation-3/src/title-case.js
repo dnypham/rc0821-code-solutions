d557a0d68d8cf95da20da4996da502d0915c84dc
Daniel Pham <dnypham@gmail.com>
2021-10-19 16:30:24 +0000

    Work continues on 'title-case.js'.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,24 @@
 /* exported titleCase */
+
+function titleCase(title) {
+  const array1 = title.split(' ');
+  const array2 = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    const wordsArray = array1[i].split('');
+    console.log(wordsArray);
+
+    for (let x = 0; x < wordsArray.length; x++) {
+      if (x === 0) {
+        wordsArray[x].toUpperCase();
+      } else {
+        wordsArray[x].toLowerCase();
+      }
+    }
+    const newWord = wordsArray.join('');
+    array2.push(newWord);
+  }
+
+  const newTitle = array2.join(' ');
+  console.log(newTitle);
+}
