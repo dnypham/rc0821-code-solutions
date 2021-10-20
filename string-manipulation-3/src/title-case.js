ccfe79e4771830a131fe8ffd3efd737b98b2f258
Daniel Pham <dnypham@gmail.com>
2021-10-20 16:48:27 +0000

    Made progress on 'title-case.js' problem. Work continues.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,23 +2,58 @@
 
 function titleCase(title) {
   const array1 = title.split(' ');
-  const array2 = [];
 
-  for (let i = 0; i < array1.length; i++) {
-    const wordsArray = array1[i].split('');
-    console.log(wordsArray);
+  const solution = array1.map((word, index) => {
 
-    for (let x = 0; x < wordsArray.length; x++) {
-      if (x === 0) {
-        wordsArray[x].toUpperCase();
-      } else {
-        wordsArray[x].toLowerCase();
-      }
+    if ((!word.includes('-')) && (!word.includes(':')) && (index !== 0) && (word === 'in' || word === 'and' || word === 'or' || word === 'nor' || word === 'but' || word === 'a' || word === 'an' || word === 'the' || word === 'as' || word === 'at' || word === 'by' || word === 'for' || word === 'of' || word === 'on' || word === 'per' || word === 'to')) {
+      return word;
+    } else if ((!word.includes('-')) && (!word.includes(':')) && (word === 'javascript' || word === 'JavaScript')) {
+      return 'JavaScript';
+    } else if ((!word.includes('-')) && (!word.includes(':')) && (word === 'api' || word === 'API')) {
+      return 'API';
+    } else if (word.includes('-')) {
+      const hyphenArray = word.split('-');
+      const test1 = hyphenArray.map((word, index) => {
+
+        const lettersArray = word.split('');
+
+        const solution2 = lettersArray.map((letter, index) => {
+
+          if (index === 0) {
+            return letter.toUpperCase();
+          } else {
+            return letter.toLowerCase();
+          }
+        });
+
+        console.log(solution2);
+        const test2 = solution2.join('');
+        console.log(test2);
+        return test2;
+      });
+      console.log(test1);
+      test1.splice(1, 0, '-');
+      console.log(test1);
+      return test1.join('');
+
     }
-    const newWord = wordsArray.join('');
-    array2.push(newWord);
-  }
 
-  const newTitle = array2.join(' ');
-  console.log(newTitle);
+    const lettersArray = word.split('');
+
+    const solution2 = lettersArray.map((letter, index) => {
+
+      if (index === 0) {
+        return letter.toUpperCase();
+      } else {
+        return letter.toLowerCase();
+      }
+    });
+
+    return solution2.join('');
+  });
+
+  const solution3 = solution.join(' ');
+  console.log(solution3);
+  return solution3;
+
 }
