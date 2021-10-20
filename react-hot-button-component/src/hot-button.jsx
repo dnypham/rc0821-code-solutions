d5acb838dd168c76ba2029e34227b56151bcede5
Daniel Pham <dnypham@gmail.com>
2021-10-20 19:52:11 +0000

    Completed react-hot-button-component exercise.

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class HotButton extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
     this.state = { clicks: 0 };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.setState({ clicks: clicks + 1 });
+    this.setState({ clicks: this.state.clicks + 1 });
   }
+
+  render() {
+    const clicks = this.state.clicks;
+    let btnColor;
+
+    if (clicks < 3) {
+      btnColor = 'btn-0';
+    } else if (clicks < 6) {
+      btnColor = 'btn-3';
+    } else if (clicks < 9) {
+      btnColor = 'btn-6';
+    } else if (clicks < 12) {
+      btnColor = 'btn-9';
+    } else if (clicks < 15) {
+      btnColor = 'btn-12';
+    } else if (clicks < 18) {
+      btnColor = 'btn-15';
+    } else if (clicks >= 18) {
+      btnColor = 'btn-18';
+    }
+
+    return (
+      <button className={`btn ${btnColor}`} onClick={this.handleClick}>Hot Button</button>
+    );
+  }
+
 }
+
+export default HotButton;
