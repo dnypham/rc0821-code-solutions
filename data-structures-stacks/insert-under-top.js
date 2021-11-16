c07fe563a8d456b9552661edb9dc360d34acfa00
Daniel Pham <dnypham@gmail.com>
2021-11-16 18:41:38 +0000

    Finish 'insert-under-top.js'.

--- data-structures-stacks/insert-under-top.js
+++ data-structures-stacks/insert-under-top.js
@@ -1,5 +1,9 @@
 /* exported insertUnderTop */
 
 function insertUnderTop(stack, value) {
-
+  if (stack.peek() !== undefined) {
+    const first = stack.pop();
+    stack.push(value);
+    stack.push(first);
+  }
 }
