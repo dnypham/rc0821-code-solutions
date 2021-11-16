b5de5600e0572de2daf50444e025e1cd9dc85848
Daniel Pham <dnypham@gmail.com>
2021-11-16 18:56:26 +0000

    Completed data-structures-stacks exercise.

--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -1,5 +1,16 @@
 /* exported maxValue */
 
 function maxValue(stack) {
+  let highestNumber = -Infinity;
 
+  while (stack.peek() !== undefined) {
+    if (stack.peek() > highestNumber) {
+      highestNumber = stack.peek();
+      stack.pop();
+    } else {
+      stack.pop();
+    }
+  }
+
+  return highestNumber;
 }
