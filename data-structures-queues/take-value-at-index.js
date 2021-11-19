c2c3c54c3aa36dbef89472f54951a9183d7c1069
Daniel Pham <dnypham@gmail.com>
2021-11-19 01:37:13 +0000

    Solve 'take-value-at-index.js'.

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,5 +1,21 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
+  let count = 0;
 
+  while (queue.peek() !== undefined) {
+    const value = queue.dequeue();
+
+    if (value === undefined) {
+      return undefined;
+    }
+
+    if (count === index) {
+      return value;
+    } else {
+      queue.enqueue(value);
+    }
+
+    count++;
+  }
 }
