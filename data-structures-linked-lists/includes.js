cf7692f197215ff1c607673fa22f8c3480eede6c
Daniel Pham <dnypham@gmail.com>
2021-11-23 18:23:24 +0000

    Completed data-structures-linked-lists exercise.

--- data-structures-linked-lists/includes.js
+++ data-structures-linked-lists/includes.js
@@ -1,5 +1,12 @@
 /* exported includes */
 
 function includes(list, value) {
+  while (list) {
+    if (list.data === value) {
+      return true;
+    }
+    list = list.next;
 
+  }
+  return false;
 }
