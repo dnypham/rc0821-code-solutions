30cd3fbcc8ccd554eecfd3711d979fef2c6d217b
Daniel Pham <dnypham@gmail.com>
2021-11-23 01:47:18 +0000

    Completed data-structures-queues exercise.

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -1,5 +1,22 @@
 /* exported takeNextSmallest */
 
 function takeNextSmallest(queue) {
+  if (queue.peek() !== undefined) {
+    let first = queue.dequeue();
+    let second = queue.peek();
 
+    if (!second) {
+      return first;
+    }
+
+    while (first > second) {
+      queue.enqueue(first);
+      first = queue.dequeue();
+      second = queue.peek();
+    }
+
+    return first;
+  } else {
+    return undefined;
+  }
 }
