bab20af618f495fe2b8d75a5ff1ec619b867c845
Daniel Pham <dnypham@gmail.com>
2022-03-02 22:02:11 +0000

    Completed 'react-accordion-component' exercise.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -7,44 +7,32 @@ export default class Accordion extends React.Component {
       id: null
     };
 
-    this.topicArray = [
-      {
-        topic: 'Hypertext Markup Language',
-        text: 'Hypertext Markup Language (HTML) is the standard markup language is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, forms a triad of cornerstone technologies for the World Wide Web.',
-        id: 1
-      },
-      {
-        topic: 'Cascading Style Sheets',
-        text: 'Cascading Style Sheets (CSS) is a style sheet language for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web alongside HTML and JavaScript',
-        id: 2
-      }
-    ];
-
     this.renderTopics = this.renderTopics.bind(this);
   }
 
   renderTopics() {
-    return this.topicArray.map(topic => (
-      <>
-      <button className='topic-button lato-400' key={topic.id} onClick={() => this.setState({ id: topic.id })}>{topic.topic}</button>
-      {this.renderText()}
-      </>
+    return this.props.topics.map(topic => (
+      <div key={topic.id}>
+        <button className='topic-button lato-400' onClick={() => this.toggleText(topic.id)}>{topic.topic}</button>
+        <div className={`topic-text-container lato-300 ${this.state.id === topic.id ? '' : 'hidden'}`}>
+          <p>{topic.text}</p>
+        </div>
+      </div>
     ));
   }
 
-  renderText() {
-    if (this.state.id !== null) {
-      const [topic] = this.topicArray.filter(topic => this.state.id === topic.id);
-
-      console.log(topic.text);
-    }
+  toggleText(id) {
+    this.state.id === id
+      ? this.setState({ id: null })
+      : this.setState({ id: id });
   }
 
   render() {
-    console.log(this.state);
     return (
       <div className='container'>
-        {this.renderTopics()}
+        <div className='topic-container'>
+          {this.renderTopics()}
+        </div>
       </div>
     );
   }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
+const topicArray = [
+  {
+    topic: 'Hypertext Markup Language',
+    text: `Hypertext Markup Language (HTML) is the standard markup language
+               is the standard markup language for creating web pages and web
+               applications. With Cascading Style Sheets (CSS) and JavaScript,
+               forms a triad of cornerstone technologies for the World Wide Web.`,
+    id: 1
+  },
+  {
+    topic: 'Cascading Style Sheets',
+    text: `Cascading Style Sheets (CSS) is a style sheet language for
+               describing the presentation of a document written in a markup
+               language like HTML. CSS is a cornerstone technology of the World
+               Wide Web alongside HTML and JavaScript`,
+    id: 2
+  },
+  {
+    topic: 'JavaScript',
+    text: `JavaScript, often abbreviated as JS, is a high-level, interpreted
+               programming language that conforms to the ECMAScript specification.
+               JavaScript has curly-bracket syntax. dynamic typing, prototype-based
+               object-orientation and first-class functions.`,
+    id: 3
+  }
+];
+
 ReactDOM.render(
-  <Accordion />,
+  <Accordion topics={topicArray} />,
   document.querySelector('#root')
 );
