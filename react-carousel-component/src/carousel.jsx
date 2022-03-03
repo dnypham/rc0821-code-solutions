47eae19c0baf4c13acff4f8dc22a068b19990a18
Daniel Pham <dnypham@gmail.com>
2022-03-03 16:48:19 +0000

    Created image carousel. Work continues.

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: 1
+    };
+  }
+
+  componentDidMount() {
+    clearInterval(this.carousel);
+  }
+
+  timer() {
+    clearInterval(this.carousel);
+
+    this.carousel = setInterval(() => {
+      if (this.state.id === this.props.pokemon.length) {
+        this.setState({ id: 1 });
+      } else {
+        this.setState({ id: this.state.id + 1 });
+      }
+    }, 3000);
+  }
+
+  renderIcons() {
+    return this.props.pokemon.map(pokemon => (
+      <i className={`fa-solid fa-circle icons ${pokemon.id === this.state.id ? 'active' : ''}`} key={pokemon.id} onClick={() => this.setState({ id: pokemon.id })}></i>
+    ));
+  }
+
+  getImage() {
+    this.timer();
+
+    const [pokemon] = this.props.pokemon.filter(pokemon => this.state.id === pokemon.id);
+
+    return pokemon.image;
+  }
+
+  arrowBack(id) {
+    if (id === 1) {
+      this.setState({ id: 5 });
+    } else {
+      this.setState({ id: id - 1 });
+    }
+  }
+
+  arrowForward(id) {
+    if (id === this.props.pokemon.length) {
+      this.setState({ id: 1 });
+    } else {
+      this.setState({ id: id + 1 });
+    }
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='content-container'>
+          <div className='arrow-container'>
+            <i className="fa-solid fa-circle-arrow-left fa-2x icons" onClick={() => this.arrowBack(this.state.id)}></i>
+          </div>
+          <div className='image-container'>
+            <img className='image' src={this.getImage()}></img>
+            <div className='dot-container'>
+              {this.renderIcons()}
+            </div>
+          </div>
+          <div className='arrow-container'>
+            <i className="fa-solid fa-circle-arrow-right fa-2x icons" onClick={() => this.arrowForward(this.state.id)}></i>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
