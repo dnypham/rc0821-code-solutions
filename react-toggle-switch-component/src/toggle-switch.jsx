efb99d152f24766990c3b32b62f3712d2750f689
Daniel Pham <dnypham@gmail.com>
2022-04-05 16:40:24 -0700

    Refactor react-toggle-switch-component to use React Hooks.

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,59 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class ToggleSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { toggle: false };
-    this.handleClick = this.handleClick.bind(this);
-  }
+export default function ToggleSwitch() {
+  const [toggle, setToggle] = useState(false);
 
-  handleClick() {
-    this.setState({ toggle: !this.state.toggle });
-  }
-
-  slide() {
-    const toggle = this.state.toggle;
-
-    if (!toggle) {
-      return 'circle-off';
-    } else {
-      return 'circle-on';
-    }
-  }
-
-  color() {
-    const toggle = this.state.toggle;
-
-    if (!toggle) {
-      return 'toggle-off';
-    } else {
-      return 'toggle-on';
-    }
-  }
-
-  label() {
-    const toggle = this.state.toggle;
-
-    if (!toggle) {
-      return 'OFF';
-    } else {
-      return 'ON';
-    }
-  }
-
-  render() {
-
-    return (
-      <div className='toggle-container'>
-        <div className={`toggle ${this.color()}`} onClick={this.handleClick}>
-          <div className={`circle ${this.slide()}`}>
-
-          </div>
-        </div>
-        <p className="label">{this.label()}</p>
+  return (
+    <div className='toggle-container'>
+      <div className={`toggle ${toggle ? 'toggle-on' : 'toggle-off'}`} onClick={() => setToggle(!toggle)}>
+        <div className={`circle ${toggle ? 'circle-on' : 'circle-off'}`}></div>
       </div>
-    );
-  }
+      <p className="label">{toggle ? 'ON' : 'OFF'}</p>
+    </div>
+  );
 }
-
-export default ToggleSwitch;
